b1514cb17f6e639e817508f310221994904390c8
Aly <38168385+alycapps@users.noreply.github.com>
2019-09-01 12:10:28 -0400

    adding buttons to screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import NumberBtn from "./components/NumberBtn";
+import { thisExpression } from '@babel/types';
+// import { number } from 'prop-types';
+// import { render } from 'react-dom';
 
 
 export interface Props {
@@ -11,13 +14,23 @@ interface State {
     numbers: number[]
 }
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Practice Calulator</h1>
-      <NumberBtn number={1}/>
-    </div>
-  );
+class App extends Component<{}, State> {
+  state = {
+    numbers: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    // number: 2
+  }
+  
+  public render() {
+    return (
+      <div className="App">
+        <h1>Practice Calulator</h1>
+        {this.state.numbers.map(number => (
+          <NumberBtn number={number}/>
+        ))}
+      </div>
+    );
+  }
+
 }
 
 export default App;
